1859916e0aedf8809c3c11875ed156cc3c4fbcdb
sihanoor <109851512+sihanoor@users.noreply.github.com>
2022-10-23 22:23:15 +0700

    Update Email.py

--- pages/Email.py
+++ pages/Email.py
@@ -1,51 +1,60 @@
-import time
-import random
 import streamlit as st
+import pandas as pd 
+import numpy as np 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib,ssl
 
-state = st.session_state
+st.title("BD Triggers-Lead Generator Mail")
+#uploaded_file = st.file_uploader("Choose a file")
+temp_file = st.file_uploader("Enter file here!")
+if temp_file: 
+	temp_file_contents = temp_file.read()
 
-# State machine states
-if 'READY_TO_SEND' not in state:
-    state.READY_TO_SEND = False
-if 'CODE_CONFIRMED' not in state:
-    state.CODE_CONFIRMED = False
+if st.button("Save as working file"):
+    with open("ON_DISK_FILE.extension","wb") as file_handle:
+        file_handle.write(temp_file_contents)
 
-# Session values
-if 'CODE' not in state:
-    state.CODE = ''
-if 'RAND_KEY' not in state:
-    state.RAND_KEY = 0
 
-message = st.empty()
-def sendmail():
-    message.success('Message sent')
-    time.sleep(2)
-    message.empty()
+result= st.button('Click To Send Mail')
+st.write(result)
+if result:
 
-if not state.READY_TO_SEND:
-    with st.form(key='my_email_form', clear_on_submit=False):
-        email = st.text_input(label='Please enter your email address', placeholder='name@company.com')
-        if st.form_submit_button(label='send'):
-            state.READY_TO_SEND = True
+	my_email= "example@gmail.com"
+	password= "********"
 
-def _code_confirm_cb():
-    state.CODE_CONFIRMED = True
 
-if state.READY_TO_SEND and not state.CODE_CONFIRMED:
-    fixed_digits = 4
-    state.RAND_KEY = random.randrange(1111, 9999, fixed_digits)
-    message.info(f'DEBUG (code hint): {state.RAND_KEY}')
 
-if state.READY_TO_SEND:
-    with st.form(key='my_code', clear_on_submit=True):
-        state.CODE = st.text_input(label='Enter code', key='code')
-        if st.form_submit_button(label='Confirm', on_click=_code_confirm_cb):
-            print(state.CODE, state.RAND_KEY)
-            # state.CODE = state.code
-            if str(state.CODE) == str(state.RAND_KEY):
-                state.READY_TO_SEND = False
+	server = smtplib.SMTP_SSL('smtp.gmail.com' ,465)
+	server.ehlo()
+	server.login(my_email, password)
+	email_list = pd.read_excel("ON_DISK_FILE.extension") 
+	st.write(email_list)
+ 
 
-if state.CODE_CONFIRMED:
-    state.CODE_CONFIRMED = False
-    sendmail()
-    st.experimental_rerun()
+ 
+	#defining objects
+	names = email_list['Lead Generated']
+	emails = email_list['Lead generator Email']
+	subjects = email_list["Subject"]    
+	ccs=email_list['CCs']
+
+	for i in range(len(emails)):
+		name=names[i]
+		email=emails[i]
+		subject=subjects[i]
+		cc=ccs[i]
+
+		msg=MIMEMultipart()
+		msg['Subject']=subject
+		msg['From']=my_email
+		msg["To"]=email
+		msg["Cc"]=cc
+		text="Hi"
+
+		part1 = MIMEText(text, "plain")
+		msg.attach(part1)
+		
+		server.sendmail(msg["From"], msg["To"].split(",") + msg["Cc"].split(","), msg.as_string())
+	
+	server.close()
