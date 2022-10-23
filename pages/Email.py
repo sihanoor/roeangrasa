1e6434b226947a8396dd558e96d9378438e3c5af
sihanoor <109851512+sihanoor@users.noreply.github.com>
2022-10-23 22:29:53 +0700

    Update Email.py

--- pages/Email.py
+++ pages/Email.py
@@ -1,60 +1,11 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-import smtplib,ssl
 
-st.title("BD Triggers-Lead Generator Mail")
-#uploaded_file = st.file_uploader("Choose a file")
-temp_file = st.file_uploader("Enter file here!")
-if temp_file: 
-	temp_file_contents = temp_file.read()
+captions = list(range(300,1200,100))
+blob_items = [f"https://dummyimage.com/400x{w}/d4c9d4/3740bd.jpg&text=IMG_{w}" for w in captions]
 
-if st.button("Save as working file"):
-    with open("ON_DISK_FILE.extension","wb") as file_handle:
-        file_handle.write(temp_file_contents)
-
-
-result= st.button('Click To Send Mail')
-st.write(result)
-if result:
-
-	my_email= "example@gmail.com"
-	password= "********"
-
-
-
-	server = smtplib.SMTP_SSL('smtp.gmail.com' ,465)
-	server.ehlo()
-	server.login(my_email, password)
-	email_list = pd.read_excel("ON_DISK_FILE.extension") 
-	st.write(email_list)
- 
-
- 
-	#defining objects
-	names = email_list['Lead Generated']
-	emails = email_list['Lead generator Email']
-	subjects = email_list["Subject"]    
-	ccs=email_list['CCs']
-
-	for i in range(len(emails)):
-		name=names[i]
-		email=emails[i]
-		subject=subjects[i]
-		cc=ccs[i]
-
-		msg=MIMEMultipart()
-		msg['Subject']=subject
-		msg['From']=my_email
-		msg["To"]=email
-		msg["Cc"]=cc
-		text="Hi"
-
-		part1 = MIMEText(text, "plain")
-		msg.attach(part1)
-		
-		server.sendmail(msg["From"], msg["To"].split(",") + msg["Cc"].split(","), msg.as_string())
-	
-	server.close()
+for blob in blob_items:
+    c1,mid,c2 = st.columns([2,1,3])
+    c1.image(blob)
+    title = c2.text_input('Comments',key=blob+"title")
+    if c2.button('Save Me', key=blob+"save"):
+        c1.write(title)
