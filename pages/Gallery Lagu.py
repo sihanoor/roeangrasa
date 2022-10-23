dbe61f473cf2d58b17d3ffebaab7a0ca72b01f11
sihanoor <109851512+sihanoor@users.noreply.github.com>
2022-10-23 22:15:57 +0700

    Update and rename Gallery Vidio.py to Gallery Lagu.py

---
+++ pages/Gallery Lagu.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import time
+
+st.markdown("#🎵 Gallery Lagu")
+st.sidebar.markdown("# 🎵 Gallery Lagu")
+
+st.title("Tulus - Diri")
+st.video('https://youtu.be/fsGcUWiylW8')
--- pages/Gallery Vidio.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import time
-
-st.markdown("# 😃Level Emosi")
-st.sidebar.markdown("# 😃Berkenalan dengan emosi")
-
-st.video('https://youtu.be/fsGcUWiylW8')
