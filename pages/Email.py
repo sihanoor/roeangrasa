45b3267514e9f3f6fc4a580cc320954a0640d6c5
sihanoor <109851512+sihanoor@users.noreply.github.com>
2022-10-23 22:32:24 +0700

    Update Email.py

--- pages/Email.py
+++ pages/Email.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-captions = list(range(300,1200,100))
-blob_items = [f"https://dummyimage.com/400x{w}/d4c9d4/3740bd.jpg&text=IMG_{w}" for w in captions]
-
-for blob in blob_items:
-    c1,mid,c2 = st.columns([2,1,3])
-    c1.image(blob)
-    title = c2.text_input('Comments',key=blob+"title")
-    if c2.button('Save Me', key=blob+"save"):
-        c1.write(title)
+title = c2.text_input('Comments',key=blob+"title")
+if st.button('Save Me', key=blob+"save"):
+    st.write(title)
